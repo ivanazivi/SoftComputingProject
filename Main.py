f42071c56e723378324d46f0c93cf5100b0e8ace
Ivana Živić <ivanazivi22320@gmail.com>
2015-12-24 23:15:31 +0100

    Napravile smo prozor. Jeej :D

---
+++ Main.py
@@ -0,0 +1,15 @@
+import sys
+from tkinter import *
+
+# create the root window
+root = Tk()
+
+
+# modify the window
+root.title("Srpski znakovni jezik")
+root.geometry("1200x600")
+
+
+
+# Start the window's event-loop
+root.mainloop()
