a88a0b2b845fed3ee375af5e199d41cb5d5c1704
ivanazivi <ivanzivi22320@gmail.comk>
2016-01-11 15:30:18 +0100

    Ubacena kamera u gui

--- Main.py
+++ Main.py
@@ -1,40 +1,36 @@
-
-from tkinter import *
-from ttk import Frame, Button, Style
+import tkinter as tk
 import sys
 import cv2
-# create the root window
-#root = Tk()
+from PIL import Image, ImageTk
 
-
-# modify the window
-#root.title("Srpski znakovni jezik")
-#root.geometry("1200x600")
-#self.style = Style()
-#self.style.theme_use("default")
-#txt = "Izlaz iz neuronske mreze ovde"
-#text1 = Label(root, text=txt)
-
-#frame = Frame(self, relief=RAISED,  borderwidth=2)
-#frame.pack(expand=True)
-
-#text1.pack(side=BOTTOM, pady=20, expand=True)
-
-# Start the window's event-loop
-#root.mainloop()
-
-
-#set video source to the default webcam
+width, height = 800, 600
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-while(True):
-    ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#create the root window
+root = tk.Tk()
+root.title("Srpski znakovni jezik app")
+root.bind('<Escape>', lambda e: root.quit())
 
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+lmain = tk.Label(root)
+lmain.pack()
 
-cap.release()
-cv2.destroyAllWindows()
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
+
+show_frame()
+
+txt = "Izlaz iz neuronske mreze ovde"
+text1 = tk.Label(root, text=txt)
+text1.pack(side=tk.BOTTOM, pady=20, expand=True)
+
+root.mainloop()
