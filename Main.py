7e2a7234dff10a0b02f059dad7b4c3052edc3d41
Fireblume <violeta.vns@gmail.com>
2016-03-28 23:16:42 +0200

    Doradjena verzija prepoznavanja sake. Rad sa YCrCb bojama. Oduzimanje pozadine od celokupne slike.

--- Main.py
+++ Main.py
@@ -1,70 +1,94 @@
-import tkinter as tk
-import sys
-import cv2
-from PIL import Image, ImageTk
 import numpy as np
+import cv2
 
-#promenljive
-kernel = np.ones((3,3),np.uint8)
+def threshold0(image):
+    value = (3, 3)
+    blurred = cv2.GaussianBlur(image, value, 0)
+    _, image = cv2.threshold(blurred, 0, 255,
+                                    cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-width, height = 800, 600
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    return image
 
+def threshold1(image):
+    value = (25, 15)
+    blurred = cv2.GaussianBlur(image, value, 0)
+    _, image = cv2.threshold(blurred, 0, 255,
+                                    cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-#create the root window
-root = tk.Tk()
-root.title("Srpski znakovni jezik app")
-root.bind('<Escape>', lambda e: root.quit())
+    return image
 
-lmain = tk.Label(root)
-lmain.pack()
+def threshold2(image):
+    value = (25, 25)
+    blurred = cv2.GaussianBlur(image, value, 0)
+    _, image = cv2.threshold(blurred, 0, 255,
+                                    cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-def dilate(img):
-    return cv2.dilate(img,kernel, iterations=1)
+    return image
 
-def erode(img):
-    return cv2.erode(img,kernel, iterations=1)
+def subtraction(img, img1):
+    image = np.absolute(img1 - img) #pozadina - frame
+    image = image >= 0
+    image = image.astype('uint8')
+    image = image * img #sa frame se mnozilo
 
-def show_frame():
-    max_area = 200
-    min_area = 50
+    return image
+def readCam():
     _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(cv2image, (5,5),0)
-    ret, show1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    img = Image.fromarray(cv2image)
-    imgtk = ImageTk.PhotoImage(image=img)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    lmain.after(10, show_frame)
-    cv2.imshow('frame3', show1)
+    frame = cv2.flip(frame,1)
 
-    _, contours, hierarchy = cv2.findContours(show1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for i in range(len(contours)):
-        cnt=contours[i]
-        area = cv2.contourArea(cnt)
-        ci = 0
-        if(area > max_area):
-          max_area=area
-          ci=i
-          break
+    return frame
 
-    cnt=contours[ci]
+def toYCRCB(img):
+    frame1 = cv2.resize(img,(640, 480), interpolation = cv2.INTER_CUBIC)
+    frame1 = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
 
-    hull = cv2.convexHull(cnt)
-    drawing = np.zeros(frame.shape,np.uint8)
-    cv2.drawContours(drawing,[cnt],0,(0,255,0),2)
-    cv2.drawContours(drawing,[hull],0,(0,0,255),2)
-    cv2.imshow('frame',drawing)
+def findCountours(contours):
+ ci = 0
+ max_area = 5000
+ for i in range(len(contours)):
+    cnt=contours[i]
+    area = cv2.contourArea(cnt)
 
-show_frame()
+    if(area > max_area):
+      max_area=area
+      ci=i
+      break
 
-txt = "Izlaz iz neuronske mreze ovde"
 
-text1 = tk.Label(root, text=txt)
-text1.pack(side=tk.BOTTOM, pady=20, expand=True)
 
-root.mainloop()
+ cnt=contours[ci]
+ #drawing = np.zeros(frameOrig.shape,np.uint8)
+ realHandLen = cv2.arcLength(cnt, True)
+ handContour = cv2.approxPolyDP(cnt, 0.001 * realHandLen, True)
+
+ return handContour
+
+cv2.ocl.setUseOpenCL(False)
+cap = cv2.VideoCapture(0)
+pozadina = readCam()
+toYCRCB(pozadina)
+
+while(1):
+ frameOrig = readCam()
+ frame = cv2.cvtColor(frameOrig, cv2.COLOR_BGR2YCrCb)
+
+ fgmask = subtraction(frame, pozadina)
+
+ _, cr, _ = cv2.split(fgmask)
+
+ cr = threshold1(cr)
+ cv2.imshow('frameorigin',cr)
+
+ #Pronalazenje kontura
+ _, contours, hierarchy = cv2.findContours(cr,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+ handContour = findCountours(contours)
+ cv2.drawContours(frameOrig,[handContour],0,(0,255,0),1)
+
+ cv2.imshow('frame1',frameOrig)
+
+ k = cv2.waitKey(30) & 0xff
+ if k == 27:
+    break
+
+cap.release()
+cv2.destroyAllWindows()
